bc5fae07ecf9267e78875dc81b6fc401260ab598
Roman <levik19021998@gmail.com>
2020-04-20 13:49:49 +0300

    это мой первый комментарий

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// ---- toString
+
+// 1)
+console.log(typeof(String(4)));
+// 2)
+console.log(typeof("" + false));
+console.log('https://vk.com/catalog/' + 5);
+
+// ---- toNumber
+
+// 1)
+console.log(typeof(Number('5')));
+// 2)
+console.log(typeof(5 + +'5'));
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+// ---- 2 ----
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+// console.log(persone.name);
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.imp']
+// console.log(arr[0]);
+
+// ---- 1 ----
+
+// var number = 5;
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(leftBorderWidth);
